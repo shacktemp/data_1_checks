275820552285799d6f0fa95d2a64cb3cf44036d9
unknown <shacktemp@gmail.com>
2023-02-19 21:41:17 -0500

    Knowledge Check 1 - bball

---
+++ Data Check 1.py
@@ -0,0 +1,9 @@
+print('hello, world!')
+bballteams = ["cards", "cats", 'racers', 'hilltoppers', 'knights', 'norse', 'eagles', 'colonels']
+print("The team of the Week:")
+print(bballteams[0])
+gameperiods_dict = {"women":4, "men":2}
+print("Women's Game Periods\n", gameperiods_dict['women'])
+print("The Next Opponent:")
+otherteams = ('tide', 'blue devils', 'tigers', 'bulldogs')
+print(otherteams[2])
